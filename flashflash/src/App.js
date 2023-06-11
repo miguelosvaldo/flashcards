d7892c913e7c46256d9adddea8186d18dc0d3182
miguelosvaldo <miguel.o.molina@icloud.com>
2023-06-10 19:44:27 -0700

    here broskies

---
+++ flashflash/src/App.js
@@ -0,0 +1,23 @@
+import react from 'react'
+import './App.css';
+// import Footer from './components/footer';
+
+
+function App() {
+  return (
+   <>
+   <h1 className='h1'>Flashcards</h1>
+   <div className="grid-container">
+      <div className="box">Unit 1</div>
+      <div className="box">Unit 2</div>
+      <div className="box">Unit 3</div>
+      <div className="box">Unit 4</div>
+      <div className="box">Unit 5</div>
+      <div className="box">Unit 6</div>
+   </div>
+   {/* <Footer/> */}
+   </>
+  );
+}
+
+export default App;
